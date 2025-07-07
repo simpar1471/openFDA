e70a96539a15087d49590f879e74b9089e4db9c0
Simon Parker <65285181+awadafuk@users.noreply.github.com>
2025-07-07 21:44:53 +0100

    chore: Add internal error to get_openFDA_options(), add more unit tests for options

--- R/options.R
+++ R/options.R
@@ -5,12 +5,26 @@
 #' @param call A function call to use if error message required.
 #' @noRd
 get_openFDA_option <- function(option_name, call = rlang::caller_env()) {
+  if (!option_name %in% names(valid_openFDA_options)) {
+    cli_valid_openFDA_option_names <- cli::cli_vec(
+      names(valid_openFDA_options), style = list("vec-last" = " or ")
+    )
+    cli::cli_abort(
+      c("!" = "The provided `option_name` ({.val {option_name}}) is invalid.",
+        "i" = "`option_name` must be one of {.val
+               {cli_valid_openFDA_option_names}}.",
+        "x" = "`option_name` was {.val {option_name}}."
+      ),
+      call = call,
+      class = "openfda_invalid_option_name",
+      .internal = TRUE
+    )
+  }
   value <- rlang::peek_option(option_name)
   valid_opts <- valid_openFDA_options[[option_name]]
   if (!value %in% valid_opts) {
     cli::cli_abort(
-      message = c(
-        "!" = "The stored value for option {.var {option_name}} is invalid.",
+      c("!" = "The stored value for option {.var {option_name}} is invalid.",
         "i" = "The option {.var {option_name}} must be one of {.val
                {valid_opts}}",
         "x" = "The value for option {.var {option_name}} was {.val {value}}."
--- tests/testthat/test-options.R
+++ tests/testthat/test-options.R
@@ -1,6 +1,6 @@
 rlang::push_options(openFDA.paging = "ask",
-                      openFDA.paging_verbosity = "verbose",
-                      openFDA.handle_http_errors = "warn")
+                    openFDA.paging_verbosity = "verbose",
+                    openFDA.handle_http_errors = "warn")
 
 test_that("You can get/set options", {
   expect_equal(rlang::peek_option("openFDA.paging"),
@@ -13,15 +13,28 @@ test_that("You can get/set options", {
 })
 
 test_that(desc = "Internal code for checking options throws the right errors", {
-  rlang::push_options(openFDA.paging = "no")
   expect_error(
-    get_openFDA_option("openFDA.paging"),
-    class = "openfda_invalid_option_value"
+    get_openFDA_option(option_name = "bad-option-name"),
+    class = "openfda_invalid_option_name"
   )
-})
 
-# TODO: Unit tests to check that `openFDA()` behaviour responds correctly to
-#       each option change.
+  for (option in names(valid_openFDA_options)) {
+    valid_option <- sample(valid_openFDA_options[[option]], size = 1)
+
+    list(valid_option) |>
+      setNames(option) |>
+      options()
+    expect_equal(get_openFDA_option(option), valid_option)
+
+    list(paste0(valid_option, "-not_valid_anymore")) |>
+      setNames(option) |>
+      options()
+    expect_error(
+      get_openFDA_option(option),
+      class = "openfda_invalid_option_value"
+    )
+  }
+})
 
 # Set options back to default values before other tests
 rlang::push_options(openFDA.paging = "ask",
